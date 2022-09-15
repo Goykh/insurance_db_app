4b40f4ae6043906fa71e856de2a76a61212e7620
Geronymo Fuhrmann <geronymo.fuhrmann@gmail.com>
2022-09-15 10:55:45 +0200

    ValueError catch for the user input.

--- main.py
+++ main.py
@@ -29,68 +29,74 @@ Vyberte si akci:
 5. Ukončit aplikaci
             """)
 
-            response = int(input())
-            print()
-
-            # Calls method to return list of all entries in DB
-            if response == 1:
-                for i in database.get_insured():
-                    print(i, "\n")
-
-            # Calls method to add one entry
-            elif response == 2:
-                print("Křestní jméno pojištěnce?")
-                first_name = input()
-                print("Příjmení pojištěnce?")
-                last_name = input()
-                print("Věk pojištěnce (číslem)?")
-                age = int(input())
-                print("Telefonní číslo pojištěnce?")
-                phone_number = int(input())
+            try:
+                response = int(input())
                 print()
-                database.add_insured(InsuredPerson(first_name, last_name, age, phone_number))
 
-            # Calls method to search for entry by first_name and last_name
-            elif response == 3:
-                print("Zadejte křestní jméno: ")
-                first_name = input().strip(" ").capitalize()
-                print("Zadejte příjmení: ")
-                last_name = input().strip(" ").capitalize()
-                print()
-                print(database.get_insured_by_name(first_name, last_name))
+                # Calls method to return list of all entries in DB
+                if response == 1:
+                    for i in database.get_insured():
+                        print(i, "\n")
 
-            # Calls method to delete entry by first_name and last_name
-            elif response == 4:
-                print("Křestní jméno pojištěného?")
-                first_name = input()
-                print("Příjmení pojištěného?")
-                last_name = input()
-                print()
-                print(f"Chystáte se vymazat pojištěného {first_name} {last_name}.")
-                # Asks for verification of the deletion to avoid a mistake delete.
-                # I am sure this can be done better, but it is the best I came up with.
-                print("Jste si tímto krokem jistý/á? (stiskněte a pro potrvzení a n pro návrat do menu)")
-                answer = input()
-                if answer == "a":
-                    # Deletes the entry
-                    print(database.delete_insured(first_name, last_name))
-                elif answer == "n":
-                    print("Proces smazání byl zrušen.")
-                    # Continues the program without deleting anything
+                # Calls method to add one entry
+                elif response == 2:
+                    print("Křestní jméno pojištěnce?")
+                    first_name = input()
+                    print("Příjmení pojištěnce?")
+                    last_name = input()
+                    print("Věk pojištěnce (číslem)?")
+                    age = int(input())
+                    print("Telefonní číslo pojištěnce?")
+                    phone_number = int(input())
+                    print()
+                    database.add_insured(InsuredPerson(first_name, last_name, age, phone_number))
+
+                # Calls method to search for entry by first_name and last_name
+                elif response == 3:
+                    print("Zadejte křestní jméno: ")
+                    first_name = input().strip(" ").capitalize()
+                    print("Zadejte příjmení: ")
+                    last_name = input().strip(" ").capitalize()
+                    print()
+                    print(database.get_insured_by_name(first_name, last_name))
+
+                # Calls method to delete entry by first_name and last_name
+                elif response == 4:
+                    print("Křestní jméno pojištěného?")
+                    first_name = input()
+                    print("Příjmení pojištěného?")
+                    last_name = input()
+                    print()
+                    print(f"Chystáte se vymazat pojištěného {first_name} {last_name}.")
+                    # Asks for verification of the deletion to avoid a mistake delete.
+                    # I am sure this can be done better, but it is the best I came up with.
+                    print("Jste si tímto krokem jistý/á? (stiskněte a pro potrvzení a n pro návrat do menu)")
+                    answer = input()
+                    if answer == "a":
+                        # Deletes the entry
+                        print(database.delete_insured(first_name, last_name))
+                    elif answer == "n":
+                        print("Proces smazání byl zrušen.")
+                        # Continues the program without deleting anything
+                    else:
+                        # Safety net if someone managed to somehow input something wrong
+                        print("Zadaný příkaz je neplatný.")
+
+                # Prints message to end the app and waits 2 seconds before it closes
                 else:
-                    # Safety net if someone managed to somehow input something wrong
-                    print("Zadaný příkaz je neplatný.")
+                    print("Děkujeme za použití, aplikace se teď ukončí.")
+                    time.sleep(2)
+                    break
 
-            # Prints message to end the app and waits 2 seconds before it closes
-            else:
-                print("Děkujeme za použití, aplikace se teď ukončí")
-                time.sleep(2)
-                break
+                # Asks to press "Enter" to return back to the menu
+                input("Pro návrat do menu stiskněte Enter.")
+                # Cleans the cmd lines before it prints the menu again
+                self.clear_screen()
+            except ValueError:
+                print("Špatný input. Napište číslo od 1 do 5.\nEnter pro návrat do menu.")
+                input()
 
-            # Asks to press "Enter" to return back to the menu
-            input("Pro návrat do menu stiskněte Enter.")
-            # Cleans the cmd lines before it prints the menu again
-            self.clear_screen()
+                self.clear_screen()
 
     def clear_screen(self):
         """
